b12feb49ba3f8f2c8e4996a57577809f7de3b032
joespinoza11 <joespinozapa@est.utn.ac.cr>
2025-11-27 13:01:06 -0600

    Creación de Paquetes
    Se creo la clase Producción pero falta programarla y se agrego la libreria jcalender para el apartado de fechas en las GUI

---
+++ src/Modelo/Produccion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author je110
+ */
+public class Produccion {
+    
+   private int idProduccion;
+   private int cultivoID;
+   private LocalDate fecha;
+   private BigDecimal cantidadRecolectada;
+   private String calidadProducto;
+   private String destino;
+   
+   
+   
+}
