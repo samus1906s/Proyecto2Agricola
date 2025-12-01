96d82c18616076039135cbc54c068103dba51987
joespinoza11 <joespinozapa@est.utn.ac.cr>
2025-12-01 12:57:30 -0600

    Clase Producción
    Se creo la clase produccion con sus respectivos atributos, constructores, getters y setters

--- src/Modelo/Produccion.java
+++ src/Modelo/Produccion.java
@@ -13,13 +13,71 @@ import java.time.LocalDate;
  */
 public class Produccion {
     
-   private int idProduccion;
-   private int cultivoID;
-   private LocalDate fecha;
-   private BigDecimal cantidadRecolectada;
-   private String calidadProducto;
-   private String destino;
-   
-   
+    private int idProduccion;
+    private int idCultivo;
+    private LocalDate fecha;
+    private BigDecimal cantidadRecolectada;
+    private String calidadProducto;
+    private String destino;
+
+    public Produccion() {
+    }
+
+    public Produccion(int idProduccion, int idCultivo, LocalDate fecha, BigDecimal cantidadRecolectada, String calidadProducto, String destino) {
+        this.idProduccion = idProduccion;
+        this.idCultivo = idCultivo;
+        this.fecha = fecha;
+        this.cantidadRecolectada = cantidadRecolectada;
+        this.calidadProducto = calidadProducto;
+        this.destino = destino;
+    }
+
+    public int getIdProduccion() {
+        return idProduccion;
+    }
+
+    public int getIdCultivo() {
+        return idCultivo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public BigDecimal getCantidadRecolectada() {
+        return cantidadRecolectada;
+    }
+
+    public String getCalidadProducto() {
+        return calidadProducto;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setIdProduccion(int idProduccion) {
+        this.idProduccion = idProduccion;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCantidadRecolectada(BigDecimal cantidadRecolectada) {
+        this.cantidadRecolectada = cantidadRecolectada;
+    }
+
+    public void setIdCultivo(int idCultivo) {
+        this.idCultivo = idCultivo;
+    }
+
+    public void setCalidadProducto(String calidadProducto) {
+        this.calidadProducto = calidadProducto;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
    
 }
