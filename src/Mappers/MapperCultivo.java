3de6f33870de8eafd2cf5a29cfe58b2d11b10b66
Brandon Valdelomar <brvaldelomarco@est.utn.ac.cr>
2025-12-01 23:50:18 -0600

    MapperCultivo
    Mapper de cultivos implementado.

---
+++ src/Mappers/MapperCultivo.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Mappers;
+
+import DTOs.DTOCultivo;
+import Entidades.iMapper; 
+import Modelo.Cultivo;
+
+/**
+ *
+ * @author Valdelomaar
+ */
+public class MapperCultivo implements iMapper<Cultivo, DTOCultivo> {
+   
+    @Override
+    public DTOCultivo ToDto(Cultivo entidad) {
+        if (entidad == null) return null;
+        
+        return new DTOCultivo(
+                entidad.getIdCultivo(),
+                entidad.getNombre(),
+                entidad.getTipo(),
+                entidad.getAreaSembrada(),
+                entidad.getEstado(),
+                entidad.getFechaSiembra(),
+                entidad.getFechaCosecha()
+        );
+    }
+
+    @Override
+    public Cultivo ToEntidad(DTOCultivo dto) {
+        if (dto == null) return null;
+        
+        return new Cultivo(
+                dto.getIdCultivo(),
+                dto.getNombre(),
+                dto.getTipo(),
+                dto.getAreaSembrada(),
+                dto.getEstado(),
+                dto.getFechaSiembra(),
+                dto.getFechaCosecha()
+        );
+    }
+    
+}
